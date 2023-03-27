c353ea7fe167a93e53fb747b68d64bff1e38f8ce
Jonathas Oliveira <jonnutri@gmail.com>
2023-03-27 14:59:20 -0300

    Exercicioso

--- bootcamps/1-Potencia-Tech-Angular-Developer-Powered-by-iFood/1-primeiras-paginas-interativas-javascript/6-arrays-estruturas-repeticao/materia.js
+++ bootcamps/1-Potencia-Tech-Angular-Developer-Powered-by-iFood/1-primeiras-paginas-interativas-javascript/6-arrays-estruturas-repeticao/materia.js
@@ -38,3 +38,22 @@ for (let index = 0; index < array.length; index += 1) {
   console.log(array[index]);
 } // João. José.
 
+// ============================================ EXERCICIOS ============================================
+
+// 1 - Crie um programa que, dado um número, imprima a sua tabuada.
+
+let number = 2;
+
+for (let i = 0; i <= 10; i ++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+
+// 2 - Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
+
+const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+for (let i = 0; i < lista.length; i += 1) {
+  if (lista[i] % 2 === 0) {
+    console.log(lista[i])
+  }
+}
